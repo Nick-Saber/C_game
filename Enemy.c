3816fa20d85719d64d75996fe9084d515b3f4a8e
AlexHSaber <ahsaber@ucla.edu>
2016-12-19 15:40:49 -0800

    Added Player and Enemy Functionality

---
+++ Enemy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int Create_Enemy( char *grid ) {
+    grid[10*20] = "\\";
+    grid[10*21] = "/";
+    return 0;
+}
+
+int Move_Enemy() {
+	return 0;
+}
---
+++ Player.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int Create_Player( char *grid ) {
+    grid[10*20] = "/";
+    grid[10*21] = "\\";
+    return 0;
+}
+
+int Move_Player() {
+	return 0;
+}
