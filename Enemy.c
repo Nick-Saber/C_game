5659d7f6340db16e635fb4735e14a11842eeadfc
AHSaber <asaber@caltropcorp.com>
2016-12-22 15:39:43 -0800

    Added the player and enemy move functionsand x,y coordinates system

--- Enemy.c
+++ Enemy.c
@@ -3,17 +3,10 @@
 typedef struct Enemy {
 	int x_pos;
 	int y_pos;
+	char character = '_';
 } Enemy;
 
-void init_Enemy(Enemy *enemy) {
-	enemy->x_pos = 25;
-	enemy->y_pos = 1;
-}
-
-void spawn_Enemy(Enemy *enemy, char **grid ) {
-	grid[enemy->y_pos][enemy->x_pos] =  '_';
-}
-
-int Move_Enemy() {
-	return 0;
+void Move_Enemy(Enemy *enemy, int x, int y) {
+	enemy->x_pos = x;
+	enemy->y_pos = y;
 }
--- Player.c
+++ Player.c
@@ -3,16 +3,11 @@
 
 typedef struct Player {
 	int x_pos;
+	int y_pos;
+	char character = '^';
 } Player;
 
-void init_Player(Player *player) {
-	player->x_pos = 25;
-}
-
-void spawn_Player(Player *player, char **grid) {
-	grid[24][player->x_pos] = 'M';
-}
-
-void move_Player(Player *player) {
-	
+void move_Player(Player *player, int x, int y) {
+	player->x_pos = x;
+	player->y_pos = y;
 }
