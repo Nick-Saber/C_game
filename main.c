acc3fb0fcf07b258f65d076c968832088467a488
Nicholas Saber <nsaber@berkeley.edu>
2016-12-22 15:30:24 -0800

    changed screen set up so it uses ncurses

--- main.c
+++ main.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
 #include <time.h>
 #include "Player.c"
 #include "Enemy.c"
 
+#define FALSE 0
+#define TRUE 1
+
 #define ncols 50
 #define nrows 25
+
 
 int display_Grid(char ** grid);
 
 int main () 
 {
 
-	//create 2d ncols x nrows grid array and fill it with empty space characters
-	char ** grid = malloc(sizeof(char *)*nrows);
 
-	for ( int i = 0; i<nrows; i++){
-		grid[i]= (char*) malloc(ncols);
-		memset(grid[i],' ',ncols);
-	}
 
 	//Initializing Player inside of the grid
 	Player player_1;
 	init_Player(&player_1);
-	spawn_Player(&player_1, grid); 
+
 	//Initializing a single enemy inside of the grid
 	Enemy enemy_1;
 	init_Enemy(&enemy_1);
-	spawn_Enemy(&enemy_1, grid);
 
+
+	//create window, s.t. key inputs are allowed but not displayed
+	initscr();
+	cbreak();
+	noecho();
+	curs_set(FALSE);
+	keypad(stdscr,TRUE);
+
+	//max x and y coordinates to deal with resizing
+	int max_x=0;
+	int max_y=0;
+
+	//start game
 	for(;;){
 
-	if(((clock()/CLOCKS_PER_SEC)%1 == 0) || ((clock()/CLOCKS_PER_SEC)%1 == 0.5))
-	{
-		display_Grid(grid);
-	}
+		getmaxyx(stdscr,max_y,max_x);
+
+		//refresh screen every 1/2 second
+		if(((clock()/CLOCKS_PER_SEC)%1 == 0) || ((clock()/CLOCKS_PER_SEC)%1 == 0.5))
+			{
+			clear();
+			mvprintw(max_y-1,max_x/2,"^");
+			refresh();
+			}
 
 	}
 	return 0;
