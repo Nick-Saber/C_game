c4bdd637c86903414fab0ab012e458b0caf29721
Nicholas Saber <nsaber@berkeley.edu>
2016-12-22 17:31:19 -0800

    added ammo size to enemy and player struct

--- Enemy.c
+++ Enemy.c
@@ -2,6 +2,7 @@ typedef struct Enemy {
 	int x_pos;
 	int y_pos;
 	char * character;
+	int ammo_size;
 	bullet ** ammo;
 
 } Enemy;
--- Player.c
+++ Player.c
@@ -2,7 +2,9 @@ typedef struct Player {
 	int x_pos;
 	int y_pos;
 	char * character;
+	int ammo_size;
 	bullet ** ammo;
+
 
 } Player;
 
