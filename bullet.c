432c7bc8d840f90cb80fe882ae0b042e2c907a11
Nicholas Saber <nsaber@berkeley.edu>
2016-12-26 08:44:33 -0800

    maded preliminary version of update_bullets function in bullet.c

--- bullet.c
+++ bullet.c
@@ -6,19 +6,45 @@ typedef struct Bullet {
 	bool is_shot;
 } Bullet;
 
+//updates bullet positions for player struct
 bool update_Bullets(Player * plyr){
 	//boolean value returned at end, TRUE if one bullet is freed, FALSE otherwise
 	bool freed = FALSE;
 
-	for(int i =0; i<plyr->ammo_size;i++){
+	for(int i =0; i<plyr->ammo_size;i++)
+		{
 		//get i'th bullet pointer in ammo array
 		Bullet * bullt = (plyr->ammo + i*sizeof(Bullet *));
-		if( plyr->friendly){
-			bullt
+
+		//update procedure for friendly player bullets
+		if(plyr->friendly)
+			{
+			//check if bullet has reached end of screen for friendly players
+			if(bullt->y_pos == 0)
+				{
+				bullt->is_shot= FALSE;
+				freed=TRUE;
+				} 
+			else//update bullet position otherwise
+				{ 
+				bullt->y_pos++;
+				}
+			}
+
+		//update procedure for enemy player bullets	
+		if(!(plyr->friendly))
+			{
+		//check if bullet has reached end of screen for enemies
+			if(bullt->y_pos == max_y-1)
+				{
+				bullt->is_shot= FALSE;
+				freed=TRUE;
+				} 
+			else
+				{ //update bullet position otherwise
+				bullt->y_pos--;
+				}
+			}
 		}
-
-
-
+	return freed;
 	}
-
-}
