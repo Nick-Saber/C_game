c295eccbb240ded4dd48ea64bf3356a69f5014a9
Nicholas Saber <nsaber@berkeley.edu>
2017-01-01 18:14:42 -0800

    added partial resizing capabilities to playmode function, one can change window size while characters y position will reflect this but not their x position, display window also resizes properly

--- main.c
+++ main.c
@@ -39,18 +39,35 @@ for(;;){
 
 static int playmode(int level){
 	//create window, s.t. key inputs are allowed but not displayed
+	// also make variables to record stdscr max x and y, past and present
+	//, these variables allow us to detect screen resizing and change display accordingly
 	initscr();
 	cbreak();
 	noecho();
 	curs_set(FALSE);
-	WINDOW * wndw=stdscr;
-	keypad(wndw,TRUE);
+	int past_scr_x;
+	int past_scr_y;
+	getmaxyx(stdscr,past_scr_y,past_scr_x);
+	int pres_scr_x=past_scr_x;
+	int pres_scr_y=past_scr_y;
+
+	int dsp_wndw_size = 1;
+	//make upper play window where game takes place
+	WINDOW * play_wndw=newwin(pres_scr_y-dsp_wndw_size,pres_scr_x, 0,0);
+	keypad(play_wndw,TRUE);
 	halfdelay(1);
 
-	//max x and y coordinates to deal with initial positioning
-	int max_x=0;
-	int max_y=0;
-	getmaxyx(wndw,max_y,max_x);
+	//make display window where things like ammo, level, and score are displayed
+	WINDOW * dsp_wndw=newwin(dsp_wndw_size,pres_scr_x, pres_scr_y-dsp_wndw_size,0);
+	int d_max_x;
+	int d_max_y;
+	getmaxyx(dsp_wndw,d_max_y,d_max_x);
+
+
+	//max x and y coordinates to deal with initial positioning for playing window
+	int p_max_x=0;
+	int p_max_y=0;
+	getmaxyx(play_wndw,p_max_y,p_max_x);
 
 
 	//array of all players, first player is always the user
@@ -61,8 +78,8 @@ static int playmode(int level){
 
 	//Initializing Player inside of the grid
 	Player player_1;
-	player_1.x_pos=max_x/2;
-	player_1.y_pos=(max_y-2);
+	player_1.x_pos=p_max_x/2;
+	player_1.y_pos=(p_max_y-1);
 	player_1.character="^";
 	player_1.friendly=TRUE;
 	player_1.ammo_size=5;
@@ -75,7 +92,7 @@ static int playmode(int level){
 	for(int i =0; i<level;i++){
 	Player * enemy = malloc(sizeof(Player));
 
-	enemy->x_pos=i*(max_x/8);
+	enemy->x_pos=i*(p_max_x/8);
 	enemy->y_pos=floor(i/4);
 	enemy->character="V";
 	enemy->friendly=FALSE;
@@ -90,37 +107,45 @@ static int playmode(int level){
 	//key variable stores current key pressed
 	wchar_t key;
 
-
-	Bullet * temp_bullet=malloc(sizeof(Bullet));
-	temp_bullet->x_pos = max_x-1;
-	temp_bullet->y_pos = max_y -1;
-	temp_bullet->character= "f";
-	temp_bullet->is_shot=TRUE;
-	Player * temp_player = malloc(sizeof(Player));
-	temp_player->ammo=&temp_bullet;
-	temp_player->ammo_size=1;
-	temp_player->friendly=TRUE;
-
-
-	//start game
+	//FOR LOOP FOR PLAYING THE GAME
 	for(;;){
+		//check for resizing and if true update past and present dimension and resize play and display windows
+		getmaxyx(stdscr,pres_scr_y,pres_scr_x);
+		if((pres_scr_x != past_scr_x) || (pres_scr_y != past_scr_y)){
+			past_scr_x=pres_scr_x;
+			past_scr_y=pres_scr_y;	
+			// wclear(play_wndw);
+			// wclear(dsp_wndw);
+			// wrefresh(play_wndw);
+			// wrefresh(dsp_wndw);	
+			delwin(play_wndw);
+			delwin(dsp_wndw);
+			play_wndw=newwin(pres_scr_y-dsp_wndw_size,pres_scr_x, 0,0);
+			keypad(play_wndw,TRUE);
+			dsp_wndw=newwin(dsp_wndw_size,pres_scr_x, pres_scr_y-dsp_wndw_size,0);	
+		}
+
+
+
 		//refresh max x and y to deal with screen resizing
-		getmaxyx(wndw,max_y,max_x);
+		getmaxyx(play_wndw,p_max_y,p_max_x);
+		getmaxyx(dsp_wndw,d_max_y,d_max_x);
+
 
 
 		//UPDATE positioning of characters and bullets every 1/2 second
-		if((key=wgetch(wndw)) != ERR)
+		if((key=wgetch(play_wndw)) != ERR)
 		{
 			switch(key)
 			{
 				case KEY_LEFT:
-					if(player_1.x_pos > 1)
+					if(player_1.x_pos > 0)
 					{
 						player_1.x_pos-=1;
 					}
 					break;
 				case KEY_RIGHT:
-					if(player_1.x_pos < (max_x -1))
+					if(player_1.x_pos < (p_max_x))
 					{
 						player_1.x_pos+=1;
 					}
@@ -134,9 +159,19 @@ static int playmode(int level){
 
 			}
 		}
+
+
+		for(int j = 0; j<num_friendlies+num_enemies;j++){
+			Player * plyr=players[j];
+			if(plyr->friendly==TRUE){
+				plyr->y_pos=p_max_y-1;
+			} else{
+				plyr->y_pos=0;
+			}
+		}
 		//update bullets for all characters
 		for(int j =0;j<num_friendlies+num_enemies;j++){
-		update_bullets(players[j],max_y,max_x);
+		update_bullets(players[j],p_max_y,p_max_x);
 		}	
 
 		//make dead is called before is_enemy_shot so that
@@ -169,14 +204,17 @@ static int playmode(int level){
 
 
 		//DISPLAY updated positions of a players, enemies and bullets
-		wclear(wndw);
-		display_players(wndw, num_friendlies+num_enemies,players);
-		display_ammo(wndw, &player_1,max_y,max_x);
-		wrefresh(wndw);
+		wclear(play_wndw);
+		wclear(dsp_wndw);
+		display_players(play_wndw, num_friendlies+num_enemies,players);
+		display_ammo(dsp_wndw, players[0],d_max_y,d_max_x);
+		wrefresh(play_wndw);
+		wrefresh(dsp_wndw);
 
 	}
 	return 0;
 }
+
 
 static int main_menu(){
 	//create window, s.t. key inputs are allowed but not displayed
@@ -221,7 +259,7 @@ static int main_menu(){
 					//10 is ASCII value for the enter key, since KEY_ENTER is only for nurmeric keypad enter in ncurses
 				case 10:
 					if(cursor==2)
-					{
+					{	
 						return PLAY;
 					}
 					if(cursor==4)
@@ -241,9 +279,7 @@ static int main_menu(){
 		mvwprintw(wndw, 2*max_y/6,max_x/2, "PLAY" );
 		mvwprintw(wndw, 3*max_y/6,max_x/2, "INFO" );
 		mvwprintw(wndw, 4*max_y/6,max_x/2, "QUIT" );
-		mvwprintw(wndw,cursor*max_y/6,max_x/2 -3
-
-			,">");
+		mvwprintw(wndw,cursor*max_y/6,max_x/2 -3,">");
 		wrefresh(wndw);
 	}
 }
